d4d6e55c08a8ff9352a3eb1927dc32005a5a0339
Ambikagarg <64024928+Ambikagarg@users.noreply.github.com>
2023-06-29 15:22:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-require('dotenv').config();
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
-
-
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
+const server = http.createServer((req, res)=>{
+    if(req.url == "/"){
+        requests("http://api.openweathermap.org/data/2.5/weather?q=Pune&appid=e52973f69b49ffa4e1d3df3c9fa3f6bd")
+        .on('data', (chunk)=> {
+          console.log(chunk)
+        })
+        .on('end', (err)=> {
+          if (err) {
+            return console.log('connection closed due to errors', err);
+          }
+         
+          console.log('end');
+        });
+    }
 
-const replaceVal = (tempVal, orgVal) => {
-  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
-  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
-  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
-  temperature = temperature.replace("{%location%}", orgVal.name);
-  temperature = temperature.replace("{%country%}", orgVal.sys.country);
-  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
-
-  return temperature;
-};
-
-const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    requests(
-      `http://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=${process.env.APPID}`
-    )
-      .on("data", (chunk) => {
-        const objdata = JSON.parse(chunk);
-        const arrData = [objdata];
-        // console.log(arrData[0].main.temp);
-        const realTimeData = arrData
-          .map((val) => replaceVal(homeFile, val))
-          .join("");
-        res.write(realTimeData);
-        // console.log(realTimeData);
-      })
-      .on("end", (err) => {
-        if (err) return console.log("connection closed due to errors", err);
-        res.end();
-      });
-  } else {
-    res.end("File not found");
-  }
 });
-
-server.listen(8000, "127.0.0.1");
+server.listen(8001, "127.0.0.1");
